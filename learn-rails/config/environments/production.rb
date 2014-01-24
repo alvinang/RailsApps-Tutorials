081b4ea4cf79df6513e5486ebd5afb6fd9f130fa
Alvin Ang <alvinangbs@gmail.com>
2014-01-24 13:30:05 -0800

    update for heroku

--- learn-rails/config/environments/production.rb
+++ learn-rails/config/environments/production.rb
@@ -90,7 +90,8 @@ LearnRails::Application.configure do
   
   config.action_mailer.smtp_settings = {
     :address   => "smtp.mandrillapp.com",
-    :port      => 25,
+    :port      => 587,
+    enable_starttls_auto: true,
     :user_name => ENV["MANDRILL_USERNAME"],
     :password  => ENV["MANDRILL_API_KEY"]
   }
